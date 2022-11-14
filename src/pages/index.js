a3d1943913907f94b71bd14280546e038478244f
Vijay Kathole <85567517+vijukathole98@users.noreply.github.com>
2022-11-14 23:15:02 +0530

    y

--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Home } from "./Home/Home";
-export { default as Projects } from "./Projects/Projects";
-export { default as Experience } from "./Experience/Experience";
-export { default as Involvements } from "./Involvements/Involvements";
-export { default as Achievements } from "./Achievements/Achievements";
-export { default as NotFound } from "./NotFound/NotFound";
